dac1958a96f56a89902798839aed0888c546cc5a
Javontay McElroy <dinomightnetwork@gmail.com>
2019-02-20 22:43:27 -0600

    Added  svg icons into the search bar.

--- insta-clone/src/components/SearchBar.js
+++ insta-clone/src/components/SearchBar.js
@@ -24,11 +24,50 @@ class SearchBar extends React.Component {
           }}
         />
         <div className='activity-icons'>
-          <img
-            className='profile'
-            src='https://i.ibb.co/BqY0r7s/profile.png'
-            alt='profile'
-          />
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            stroke-width='2'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            class='feather feather-compass'
+          >
+            <circle cx='12' cy='12' r='10' />
+            <polygon points='16.24 7.76 14.12 14.12 7.76 16.24 9.88 9.88 16.24 7.76' />
+          </svg>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            stroke-width='2'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            class='feather feather-heart'
+          >
+            <path d='M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z' />
+          </svg>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            stroke-width='2'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            class='feather feather-user'
+          >
+            <path d='M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2' />
+            <circle cx='12' cy='7' r='4' />
+          </svg>
         </div>
       </div>
     );
